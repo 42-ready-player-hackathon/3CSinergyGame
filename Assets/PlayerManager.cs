ceb16f247ae87177a97d18eaa908f633be49fff5
edithturn <edithpuclla20@gmail.com>
2020-09-17 08:28:55 -0700

    add final feature

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour{
+
+		public int currentPoint;
+		
+		float currentTime = 0f;
+		float startingTime = 10f;
+		float addedTime = 0f;
+
+		public Text countdownText;
+		public Text pointText;
+
+		void Start(){
+				currentTime = startingTime;
+		}
+
+		void Update () {
+				currentTime -= 1 * Time.deltaTime - addedTime;
+				addedTime = 0;
+				pointText.text = currentPoint.ToString();
+				countdownText.text = currentTime.ToString("0");
+				if (currentTime <= 0){
+						countdownText.text = "Game Over";// GameOver
+				}
+		}
+
+		public void PickupItem(GameObject obj){
+				if (obj.tag == "point"){
+						currentPoint++;
+				}
+				else if (obj.tag == "mask"){
+						addedTime = 10f;
+				}
+				else if (obj.tag == "obstacle"){
+						countdownText.text = "Game Over";// GameOver
+				}
+		}
+
+}
